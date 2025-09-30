506a8f448e419d8e00b9121e8d5a5193878020c0
Pascal Schiessle <pascal.schiessle@web.de>
2025-09-30 18:25:11 +0200

    Sending color ramps

--- src/main.py
+++ src/main.py
@@ -5,6 +5,14 @@ import time
 from pyartnet import ArtNetNode
 from pyartnet.base import channel
 
+# IMPORTANT: WLED TURN ON: >>MAIN SEGMENTS ONLY<<
+
+colors= [
+    [255, 0, 0],
+    [0, 255, 0],
+    [0, 0, 255],
+]
+
 NUM_LEDS = 6
 
 async def main():
@@ -22,37 +30,20 @@ async def main():
     # the DMX values 1..3 of the universe
     channel = universe.add_channel(start=1, width= (3*NUM_LEDS) )
 
-    print("Starting cycles")
-    cycles = 0
-    while(cycles < 1000):
+    max_cycles = 1000
+    cycles = max_cycles
+    while(cycles > 0):
+        print("Starting cycle..." + str(max_cycles - cycles) + "/" + str(max_cycles) + "...")
         aggr = []
         for i in range(0, NUM_LEDS):
-            print(i)
             d = (cycles + i) % 3
-            print(d)
-            match d:
-                case 0:
-                    aggr.append(255)
-                    aggr.append(0)
-                    aggr.append(0)
-                    continue
-                case 1:
-                    aggr.append(0)
-                    aggr.append(255)
-                    aggr.append(0)
-                    continue
-                case 2:
-                    aggr.append(0)
-                    aggr.append(0)
-                    aggr.append(255)
-                    continue
+            aggr.extend(colors[d])
 
         print(aggr)
         channel.add_fade(aggr, 1000)
 
-        # this can be used to wait till the fade is complete
         await channel
         time.sleep(0.5)
-        cycles = cycles + 1
+        cycles = cycles - 1
 
 asyncio.run(main())
